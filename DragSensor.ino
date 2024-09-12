4b9bb0ef4b21ddd493e90f77a948ba2e60de466c
Lilian Labat <lilian.labat@epita.fr>
2024-09-12 12:49:36 +0200

    feat: added end sensor

--- DragSensor.ino
+++ DragSensor.ino
@@ -2,17 +2,30 @@
 #define YELLOW_TWO_LED      10
 #define RED_ONE_LED         2
 #define RED_TWO_LED         9
+#define WHITE_ONE_LED         -1
+#define WHITE_TWO_LED         -1
 
-#define SENSOR_ONE_OUT      14
-#define SENSOR_ONE_IN       12
+#define SENSOR_START_ONE_OUT      14
+#define SENSOR_START_ONE_IN       12
 
-#define SENSOR_TWO_OUT      13
-#define SENSOR_TWO_IN       15
+#define SENSOR_START_TWO_OUT      13
+#define SENSOR_START_TWO_IN       15
+
+#define USE_END_SENSOR 0
+
+#define SENSOR_END_ONE_OUT      -1
+#define SENSOR_END_ONE_IN       -1
+
+#define SENSOR_END_TWO_OUT      -1
+#define SENSOR_END_TWO_IN       -1
 
 #define START_IN            0
 
-float init_one_distance = 0;
-float init_two_distance = 0;
+float init_one_start_distance = 0;
+float init_two_start_distance = 0;
+
+float init_one_end_distance = 0;
+float init_two_end_distance = 0;
 
 float start_time = 0;
 
@@ -24,34 +37,63 @@ bool last_start = false;
 
 float get_one_distance()
 {
-  digitalWrite(SENSOR_ONE_OUT, LOW);  
+  digitalWrite(SENSOR_START_ONE_OUT, LOW);  
   delayMicroseconds(2);  
-  digitalWrite(SENSOR_ONE_OUT, HIGH);  
+  digitalWrite(SENSOR_START_ONE_OUT, HIGH);  
   delayMicroseconds(10);  
-  digitalWrite(SENSOR_ONE_OUT, LOW); 
+  digitalWrite(SENSOR_START_ONE_OUT, LOW); 
 
-  float duration = pulseIn(SENSOR_ONE_IN, HIGH);
+  float duration = pulseIn(SENSOR_START_ONE_IN, HIGH);
 
   return (duration*.0343)/2;
 }
 
 float get_two_distance()
 {
-  digitalWrite(SENSOR_TWO_OUT, LOW);
+  digitalWrite(SENSOR_START_TWO_OUT, LOW);
   delayMicroseconds(2);
-  digitalWrite(SENSOR_TWO_OUT, HIGH);
+  digitalWrite(SENSOR_START_TWO_OUT, HIGH);
   delayMicroseconds(10);
-  digitalWrite(SENSOR_TWO_OUT, LOW);
+  digitalWrite(SENSOR_START_TWO_OUT, LOW);
 
-  float duration = pulseIn(SENSOR_TWO_IN, HIGH);
+  float duration = pulseIn(SENSOR_START_TWO_IN, HIGH);
+
+  return (duration*.0343)/2;
+}
+
+float get_one_end_distance()
+{
+  digitalWrite(SENSOR_END_ONE_OUT, LOW);
+  delayMicroseconds(2);
+  digitalWrite(SENSOR_END_ONE_OUT, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(SENSOR_END_ONE_OUT, LOW);
+
+  float duration = pulseIn(SENSOR_END_ONE_IN, HIGH);
+
+  return (duration*.0343)/2;
+}
+
+float get_two_end_distance()
+{
+  digitalWrite(SENSOR_END_TWO_OUT, LOW);
+  delayMicroseconds(2);
+  digitalWrite(SENSOR_END_TWO_OUT, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(SENSOR_END_TWO_OUT, LOW);
+
+  float duration = pulseIn(SENSOR_END_TWO_IN, HIGH);
 
   return (duration*.0343)/2;
 }
 
 void reset_pos()
 {
-  init_one_distance = get_one_distance();
-  init_two_distance = get_two_distance();
+  init_one_start_distance = get_one_distance();
+  init_two_start_distance = get_two_distance();
+
+  init_one_end_distance = get_one_end_distance();
+  init_two_end_distance = get_two_end_distance();
 }
 
 void setup() {
@@ -60,25 +102,47 @@ void setup() {
   pinMode(RED_ONE_LED, OUTPUT);
   pinMode(RED_TWO_LED, OUTPUT);
 
-  pinMode(SENSOR_ONE_OUT, OUTPUT);
-  pinMode(SENSOR_ONE_IN, INPUT);
+  pinMode(SENSOR_START_ONE_OUT, OUTPUT);
+  pinMode(SENSOR_START_ONE_IN, INPUT);
 
-  pinMode(SENSOR_TWO_OUT, OUTPUT);
-  pinMode(SENSOR_TWO_IN, INPUT);
+  pinMode(SENSOR_START_TWO_OUT, OUTPUT);
+  pinMode(SENSOR_START_TWO_IN, INPUT);
 
   pinMode(START_IN, INPUT);
+
+  if (USE_END_SENSOR)
+  {
+    pinMode(WHITE_ONE_LED, OUTPUT);
+    pinMode(WHITE_TWO_LED, OUTPUT);
+
+    pinMode(SENSOR_END_ONE_OUT, OUTPUT);
+    pinMode(SENSOR_END_ONE_IN, INPUT);
+
+    pinMode(SENSOR_END_TWO_OUT, OUTPUT);
+    pinMode(SENSOR_END_TWO_IN, INPUT);
+  }
 
   reset_pos();
 }
 
 bool one_trigger()
 {
-  return get_one_distance() < (init_one_distance - 50);
+  return get_one_distance() < (init_one_start_distance - 50);
 }
 
 bool two_trigger()
 {
-  return get_two_distance() < (init_two_distance - 50);
+  return get_two_distance() < (init_two_start_distance - 50);
+}
+
+bool one_end()
+{
+  return get_two_distance() < (init_two_start_distance - 50);
+}
+
+bool two_end()
+{
+  return get_two_distance() < (init_two_start_distance - 50);
 }
 
 bool get_start()
@@ -135,13 +199,39 @@ void loop()
     if (!last_start)
     {
       start_time = millis();
+      one_time = -1;
+      two_time = -1;
     }
     last_start = true;
     triggered = false;
     digitalWrite(YELLOW_ONE_LED, LOW);
     digitalWrite(YELLOW_TWO_LED, LOW);
+    if (USE_END_SENSOR)
+    {
+      if (one_end)
+      {
+        one_time = millis() - start_time;
+        if (two_end < 0)
+        {
+          digitalWrite(WHITE_ONE_LED, HIGH);
+        }
+      }
+      if (two_end)
+      {
+        two_time = millis() - start_time;
+        if (one_end < 0)
+        {
+          digitalWrite(WHITE_TWO_LED, HIGH);
+        }
+      }
+    }
   }else
   {
     last_start = false;
+    if (USE_END_SENSOR)
+    {
+      digitalWrite(WHITE_ONE_LED, LOW);
+      digitalWrite(WHITE_TWO_LED, LOW);
+    }
   }
 }
